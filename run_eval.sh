ee1d1a6c5ce973dbc406bc824d426095a9d9ea67
ofirarviv <ofir.arviv@ibm.com>
2025-03-19 19:14:46 +0200

    add eval script

--- run_eval.sh
+++ run_eval.sh
@@ -1,7 +1,7 @@
 for task in longqa; do
   python eval.py --config configs/${task}_short.yaml \
     --model_name_or_path "meta-llama/Llama-3.1-8B" \
-    --output_dir outputs/"meta-llama_Llama-3.1-8B"/$task_short  \
+    --output_dir output/"meta-llama_Llama-3.1-8B"/$task_short  \
     --use_chat_template False # only if you are using non-instruction-tuned models, otherwise use the default.
 done
 
