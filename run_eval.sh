e676fd2470c9e855e9c400b0004057aa6f07cf83
ofirarviv <ofir.arviv@ibm.com>
2025-03-20 15:29:44 +0200

    add sample configs

--- run_eval.sh
+++ run_eval.sh
@@ -1,7 +1,14 @@
-for task in longqa; do
-  python eval.py --config configs/${task}_short.yaml \
+for task in rag; do
+  python eval.py --config configs/${task}_short_sample.yaml \
     --model_name_or_path "meta-llama/Llama-3.1-8B" \
-    --output_dir output/"meta-llama_Llama-3.1-8B"/$task_short  \
+    --output_dir output/"meta-llama_Llama-3.1-8B"/$task_sample_short  \
+    --use_chat_template False # only if you are using non-instruction-tuned models, otherwise use the default.
+done
+
+for task in rag; do
+  python eval.py --config configs/${task}_short_sample_zero_shot.yaml \
+    --model_name_or_path "meta-llama/Llama-3.1-8B" \
+    --output_dir output/"meta-llama_Llama-3.1-8B"/$task_sample_short_zero_shot  \
     --use_chat_template False # only if you are using non-instruction-tuned models, otherwise use the default.
 done
 
