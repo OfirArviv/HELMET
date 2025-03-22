ccfa904a673355c4273f2464f3b550a040f2f959
ofirarviv <ofir.arviv@ibm.com>
2025-03-22 11:25:47 +0200

    add sample configs

--- run_eval.sh
+++ run_eval.sh
@@ -1,14 +1,28 @@
 for task in rag; do
-  python eval.py --config configs/${task}_short_sample.yaml \
+  python eval.py --config configs/${task}_short_example.yaml \
     --model_name_or_path "meta-llama/Llama-3.1-8B" \
-    --output_dir output/"meta-llama_Llama-3.1-8B"/$task_sample_short  \
+    --output_dir output/"meta-llama_Llama-3.1-8B"/$task_short_example  \
     --use_chat_template False # only if you are using non-instruction-tuned models, otherwise use the default.
 done
 
 for task in rag; do
-  python eval.py --config configs/${task}_short_sample_zero_shot.yaml \
+  python eval.py --config configs/${task}_short_example_zero_shot.yaml \
     --model_name_or_path "meta-llama/Llama-3.1-8B" \
-    --output_dir output/"meta-llama_Llama-3.1-8B"/$task_sample_short_zero_shot  \
+    --output_dir output/"meta-llama_Llama-3.1-8B"/$task_short_example_zero_shot  \
+    --use_chat_template False # only if you are using non-instruction-tuned models, otherwise use the default.
+done
+
+for task in rag; do
+  python eval.py --config configs/${task}_short_example.yaml \
+    --model_name_or_path "meta-llama/Llama-3.1-8B-Instruct" \
+    --output_dir output/"meta-llama_Llama-3.1-8B-Instruct"/$task_short_example  \
+    --use_chat_template False # only if you are using non-instruction-tuned models, otherwise use the default.
+done
+
+for task in rag; do
+  python eval.py --config configs/${task}_short_example_zero_shot.yaml \
+    --model_name_or_path "meta-llama/Llama-3.1-8B-Instruct" \
+    --output_dir output/"meta-llama_Llama-3.1-8B-Instruct"/$task_short_sample_zero_shot  \
     --use_chat_template False # only if you are using non-instruction-tuned models, otherwise use the default.
 done
 
