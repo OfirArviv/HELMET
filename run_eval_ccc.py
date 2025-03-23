0bda3ad61500be4b400960547622ab9f957bcad3
ofirarviv <ofir.arviv@ibm.com>
2025-03-23 20:29:32 +0200

    update eval script

--- run_eval_ccc.py
+++ run_eval_ccc.py
@@ -5,26 +5,26 @@ from tqdm import tqdm
 from cluster_strategies import SingleRunArgs, CCCClusterStrategy
 
 instruct_model_names = [
-    # "meta-llama/Llama-3.1-8B-Instruct",
-    # "Qwen/Qwen2.5-7B-Instruct",
-    # "Qwen/Qwen2.5-7B-Instruct-1M",
-    # "google/gemma-3-4b-it",
-    # "google/gemma-3-12b-it",
-    # "google/gemma-2-9b-it",
-    # "tiiuae/Falcon3-7B-Instruct",
+    "meta-llama/Llama-3.1-8B-Instruct",
+    "Qwen/Qwen2.5-7B-Instruct",
+    "Qwen/Qwen2.5-7B-Instruct-1M",
+    "google/gemma-3-4b-it",
+    "google/gemma-3-12b-it",
+    "google/gemma-2-9b-it",
+    "tiiuae/Falcon3-7B-Instruct",
     "tiiuae/Falcon3-Mamba-7B-Instruct",
-    # "Zyphra/Zamba2-7B-Instruct-v2",
+    "Zyphra/Zamba2-7B-Instruct-v2",
 ]
 
 base_model_names = [
-    # "meta-llama/Llama-3.1-8B",
-    # "Qwen/Qwen2.5-7B",
-    # "google/gemma-3-4b-pt",
-    # "google/gemma-3-12b-pt",
-    # "google/gemma-2-9b",
-    # "tiiuae/Falcon3-7B-Base",
-    # "tiiuae/Falcon3-Mamba-7B-Base",
-    # "Zyphra/Zamba2-7B",
+    "meta-llama/Llama-3.1-8B",
+    "Qwen/Qwen2.5-7B",
+    "google/gemma-3-4b-pt",
+    "google/gemma-3-12b-pt",
+    "google/gemma-2-9b",
+    "tiiuae/Falcon3-7B-Base",
+    "tiiuae/Falcon3-Mamba-7B-Base",
+    "Zyphra/Zamba2-7B",
 ]
 
 
@@ -34,7 +34,7 @@ configs = glob.glob("configs/recall_short.yaml")
 args_list = []
 for config in configs:
     for model_name in base_model_names + instruct_model_names:
-        output_dir = f'output/{model_name.replace("/", "_").replace(".", "_").replace("-", "_")}'
+        output_dir = f'output_vllm/{model_name.replace("/", "_").replace(".", "_").replace("-", "_")}'
         use_chat_template = "True" if model_name in instruct_model_names else "False"
         args = SingleRunArgs(model_name_or_path=model_name,
                              use_chat_template=use_chat_template,
