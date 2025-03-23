09d2f3210e37f3837eb8d02c6837e725f6fd9d2f
ofirarviv <ofir.arviv@ibm.com>
2025-03-23 20:21:00 +0200

    update huggingface model loading because error in gemma

--- cluster_strategies.py
+++ cluster_strategies.py
@@ -252,7 +252,7 @@ class CCCClusterStrategy(ClusterStrategy):
             print(f"args string: {args_string}")
             python_file_to_run = "eval.py"
             command_to_run = (
-                f"{self._launch_cmd(args_strings)} {python_file_to_run} {args_string}"
+                f"{self._launch_cmd(args_strings)} {python_file_to_run} {args_string} --use_vllm"
             )
 
             print(f"\nDispatching ccc job with command: {command_to_run}")
