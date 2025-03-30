e4b5fa4feb519bfed606b0091c9620f8aa13e853
ofirarviv <ofir.arviv@ibm.com>
2025-03-30 12:42:08 +0300

    run cc jobs only if does not exissts

--- run_eval_ccc.py
+++ run_eval_ccc.py
@@ -1,8 +1,50 @@
 import glob
+import os
 
 from tqdm import tqdm
 
+from arguments import parse_arguments
 from cluster_strategies import SingleRunArgs, CCCClusterStrategy
+
+
+def does_exists(cmd_str: str):
+    args = parse_arguments(cmd_str)
+
+    assert args.model_name_or_path is not None
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    datasets = args.datasets.split(",")
+    test_files = args.test_files.split(",")
+    demo_files = args.demo_files.split(",")
+    max_lengths = ([int(args.input_max_length)] * len(datasets)) if isinstance(args.input_max_length, int) or len(
+        args.input_max_length.split(",")) == 1 else [int(l) for l in args.input_max_length.split(",")]
+    gen_lengths = ([int(args.generation_max_length)] * len(datasets)) if isinstance(args.generation_max_length,
+                                                                                    int) or len(
+        args.generation_max_length.split(",")) == 1 else [int(l) for l in args.generation_max_length.split(",")]
+    assert len(test_files) == len(demo_files)
+
+    args.input_max_length = max(max_lengths)
+
+    for dataset, test_file, demo_file, max_length, gen_length in zip(datasets, test_files, demo_files, max_lengths,
+                                                                     gen_lengths):
+        args.datasets = dataset
+        args.test_files = test_file
+        args.demo_files = demo_file
+        args.input_max_length = max_length
+        args.generation_max_length = gen_length
+
+        tag = args.tag
+        if dataset == "popqa":
+            tag += f"_pop{args.popularity_threshold}"
+
+        test_name = os.path.splitext(os.path.basename(test_file))[0]
+        output_path = os.path.join(args.output_dir,
+                                   f"{dataset}_{tag}_{test_name}_in{args.input_max_length}_size{args.max_test_samples}_shots{args.shots}_samp{args.do_sample}max{args.generation_max_length}min{args.generation_min_length}t{args.temperature}p{args.top_p}_chat{args.use_chat_template}_{args.seed}.json")
+        exists = os.path.exists(output_path)
+        if exists is False:
+            return False
+    return True
+
 
 instruct_model_names = [
     "meta-llama/Llama-3.1-8B-Instruct",
@@ -40,7 +82,13 @@ for config in configs:
                              use_chat_template=use_chat_template,
                              config=config,
                              output_dir=output_dir)
-        args_list.append(args)
+        args_strings = args.get_args_dict()
+        arg_names_and_values = [
+            f"--{arg_name} {arg_str}" for arg_name, arg_str in args_strings.items()
+        ]
+        args_string = " ".join(arg_names_and_values)
+        if not does_exists(args_string):
+            args_list.append(args)
 
 cluster_strategy = CCCClusterStrategy(
     tasks_args_list=args_list,
@@ -60,14 +108,5 @@ for res in tqdm(
             desc="Running benchmark",
         ):
     print("done")
-
-
-
-
-
-
-
-
-
 
 
