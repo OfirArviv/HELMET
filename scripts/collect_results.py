7cfead58b6452585795aab6d4ed32d51e477b8ec
ofirarviv <ofir.arviv@ibm.com>
2025-03-30 14:20:32 +0300

    fixes

--- scripts/collect_results.py
+++ scripts/collect_results.py
@@ -268,10 +268,10 @@ if __name__ == "__main__":
         "Qwen/Qwen2.5-7B-Instruct-1M",
         # "google/gemma-3-4b-it",
         # "google/gemma-3-12b-it",
-        "google/gemma-2-9b-it",
-        "tiiuae/Falcon3-7B-Instruct",
-        "tiiuae/Falcon3-Mamba-7B-Instruct",
-        "Zyphra/Zamba2-7B-Instruct-v2",
+        # "google/gemma-2-9b-it",
+        # "tiiuae/Falcon3-7B-Instruct",
+        # "tiiuae/Falcon3-Mamba-7B-Instruct",
+        # "Zyphra/Zamba2-7B-Instruct-v2",
     ]
 
     base_model_names = [
@@ -279,10 +279,10 @@ if __name__ == "__main__":
         "Qwen/Qwen2.5-7B",
         # "google/gemma-3-4b-pt",
         # "google/gemma-3-12b-pt",
-        "google/gemma-2-9b",
-        "tiiuae/Falcon3-7B-Base",
-        "tiiuae/Falcon3-Mamba-7B-Base",
-        "Zyphra/Zamba2-7B",
+        # "google/gemma-2-9b",
+        # "tiiuae/Falcon3-7B-Base",
+        # "tiiuae/Falcon3-Mamba-7B-Base",
+        # "Zyphra/Zamba2-7B",
     ]
 
     models_configs = []
@@ -319,7 +319,7 @@ if __name__ == "__main__":
     df = []
     for model in tqdm(models_configs):
         args = arguments()
-        args.tag = "v1" # SET YOUR TAG HERE
+        args.tag = "eval" # SET YOUR TAG HERE
         args.output_dir = f"output/{model['model']}"
     
         for dataset in dataset_configs:
@@ -349,7 +349,10 @@ if __name__ == "__main__":
     lf_df = lf_df.reset_index()
 
     for k, v in custom_avgs.items():
-        lf_df[k] = lf_df[v].mean(axis=1)
+        try:
+            lf_df[k] = lf_df[v].mean(axis=1)
+        except Exception:
+            print(k)
 
     print(lf_df.to_csv(index=False))
 
