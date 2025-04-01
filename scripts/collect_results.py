54d26e673f0042cd2cd1d1a32a0be9187d5bdee3
ofirarviv <ofir.arviv@ibm.com>
2025-04-01 14:03:28 +0300

    fixes

--- scripts/collect_results.py
+++ scripts/collect_results.py
@@ -275,6 +275,7 @@ if __name__ == "__main__":
     ]
 
     base_model_names = [
+        "/dccstor/eval-fms/models/instruct_models/tuluv3/2.1T_finemath/lr1e_6_finemath_wd_0_gbs_16-hf/",
         "meta-llama/Llama-3.1-8B",
         "Qwen/Qwen2.5-7B",
         # "google/gemma-3-4b-pt",
